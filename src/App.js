d8860842c46000eb5d50196b435f13e5e1984891
PokeLegoCuber <PokeLegoCuber@yahoo.com>
2020-06-03 01:21:08 -0400

    Finish customizing contact react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ContactCard from "./ContactCard";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ContactCard
+        name="Soldier Boy"
+        mobile="678-999-8212"
+        work="877-393-4448"
+        email="boy@soldier.com"
+      />
+      <ContactCard
+        name="Jill Jack"
+        mobile="718-555-5555"
+        work="718-222-2222"
+        email="jill@aol.org"
+      />
+      <ContactCard
+        name="Mike Ike"
+        mobile="347-555-5555"
+        work="555-222-2222"
+        email="mike@ike.com"
+      />
+    </>
   );
 }
 
